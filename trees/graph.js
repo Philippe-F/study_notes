07b5677c2f8e5e0f41fa3b7102db01769ee207aa
Philippe Fonzin <pfonzin@gmail.com>
2020-11-06 12:17:18 -0500

    add a node

--- trees/graph.js
+++ trees/graph.js
@@ -12,4 +12,30 @@ class Graph {
   constructor() {
     this.root = null;
   }
+
+  addNode(val, parent = null) {
+    let node = new GraphNode(val), seen = new Set();
+
+    if (!this.root) {
+      this.root = node;
+    } else if (!parent) {
+      this.root.children.push(node);
+    } else {
+      let queue = [...this.root.children];
+
+      while (queue.length) {
+        let vertex = queue.shift();
+
+        if (!seen.has(vertex.val)) {
+          if (vertex.val === parent) {
+            vertex.children.push(node);
+          } else {
+            seen.add(vertex.val)
+            queue.push(...vertex.children);
+            break;
+          }
+        }
+      }
+    }
+  }
 }
